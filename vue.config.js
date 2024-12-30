9808b024dffc26f1597c82d703a8ac7f033cd2dd
AgustinMadygraf <agustin.mtto.madygraf>
2024-12-30 19:48:29 -0300

    Add environment configuration files and update vue.config.js for public path support

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+/*
+Path: vue.config.js
+Este script se utiliza para configurar el proyecto de Vue.js.
+*/
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.VUE_APP_PUBLIC_PATH
 })
